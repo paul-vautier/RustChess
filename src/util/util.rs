8d61a1efa201647e81d05689a73b41e1203a90e1
Paul <vautier.paul14350@gmail.com>
2022-09-23 18:57:06 +0200

    Added util

---
+++ src/util/util.rs
@@ -0,0 +1,14 @@
+pub fn add_usize(value : usize, offset : i32) -> usize {
+    let val = if offset.is_negative() {
+        match value.checked_sub(offset as usize) {
+            Some(new) => new,
+            None => panic!("Index out of bounds {} - {}", value, offset),
+        }
+    } else {
+        match value.checked_add(offset as usize) {
+            Some(new) => new,
+            None => panic!("Index out of bounds {} + {}", value, offset),
+        }
+    };
+    val
+}
